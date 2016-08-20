680c5ffdac785af2e93e6c6102fe0e7fe1372302
Joseph Maynard <joseph.maynard@gmail.com>
2016-08-20 16:02:04 +1000

    Change gampa.js from object to module, tidy gampa.php formatting

--- gampa.php
+++ gampa.php
@@ -17,39 +17,39 @@
 
 	if (isset($_GET["t"])){
 
-	if(htmlspecialchars($_GET["t"]) == 'pageview'){
-		$data = array(
-			'v' => '1',
-			't' => 'pageview',
-			'tid' => htmlspecialchars($_GET["tid"]),
-			'cid' => htmlspecialchars($_GET["cid"]),
-			'dh' => htmlspecialchars($_GET["dh"]),
-			'dp' => htmlspecialchars($_GET["dp"]),
-			'dt' => htmlspecialchars($_GET["dt"]),
-			'uip' => $_SERVER['REMOTE_ADDR'],
-			'ua' => htmlspecialchars($_SERVER['HTTP_USER_AGENT'])
-		);
+		if(htmlspecialchars($_GET["t"]) == 'pageview'){
+			$data = array(
+				'v' => '1',
+				't' => 'pageview',
+				'tid' => htmlspecialchars($_GET["tid"]),
+				'cid' => htmlspecialchars($_GET["cid"]),
+				'dh' => htmlspecialchars($_GET["dh"]),
+				'dp' => htmlspecialchars($_GET["dp"]),
+				'dt' => htmlspecialchars($_GET["dt"]),
+				'uip' => $_SERVER['REMOTE_ADDR'],
+				'ua' => htmlspecialchars($_SERVER['HTTP_USER_AGENT'])
+			);
 
 
-		sendData($data);
+			sendData($data);
+		}
+
+		if(htmlspecialchars($_GET["t"]) == 'event'){
+			$data = array(
+				'v' => '1',
+				't' => 'event',
+				'tid' => htmlspecialchars($_GET["tid"]),
+				'cid' => htmlspecialchars($_GET["cid"]),
+				'ec' => htmlspecialchars($_GET["ec"]),
+				'ea' => htmlspecialchars($_GET["ea"]),
+				'uip' => $_SERVER['REMOTE_ADDR'],
+				'ua' => htmlspecialchars($_SERVER['HTTP_USER_AGENT'])
+			);
+			
+			sendData($data);
+		}
+
 	}
-
-	if(htmlspecialchars($_GET["t"]) == 'event'){
-		$data = array(
-			'v' => '1',
-			't' => 'event',
-			'tid' => htmlspecialchars($_GET["tid"]),
-			'cid' => htmlspecialchars($_GET["cid"]),
-			'ec' => htmlspecialchars($_GET["ec"]),
-			'ea' => htmlspecialchars($_GET["ea"]),
-			'uip' => $_SERVER['REMOTE_ADDR'],
-			'ua' => htmlspecialchars($_SERVER['HTTP_USER_AGENT'])
-		);
-		
-		sendData($data);
-	}
-
-}
 
 
 
@@ -58,9 +58,3 @@
 	echo "\x47\x49\x46\x38\x37\x61\x1\x0\x1\x0\x80\x0\x0\xfc\x6a\x6c\x0\x0\x0\x2c\x0\x0\x0\x0\x1\x0\x1\x0\x0\x2\x2\x44\x1\x0\x3b";
 
 ?>
-
-<!-- 
-
-https://creative.mobileembrace.com/test/analytics.php?t=pageview&tid=UA-44687118-28&cid=1471487561962&dp=%2Fhome
-
--->
